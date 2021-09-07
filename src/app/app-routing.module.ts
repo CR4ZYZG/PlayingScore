602e3f9e6d023af61e93a8e2df5d19672c16b84b
CR4ZYZG <gabrielc.ftn1@gmail.com>
2021-09-07 17:17:56 -0300

    terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,18 @@ const routes: Routes = [
   {
     path: 'perfil',
     loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'jogos',
+    loadChildren: () => import('./jogos/jogos.module').then( m => m.JogosPageModule)
+  },
+  {
+    path: 'partidas',
+    loadChildren: () => import('./partidas/partidas.module').then( m => m.PartidasPageModule)
+  },
+  {
+    path: 'marcador',
+    loadChildren: () => import('./marcador/marcador.module').then( m => m.MarcadorPageModule)
   }
 ];
 @NgModule({
